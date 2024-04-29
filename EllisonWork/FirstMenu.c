ecbf3514e2684f2d648cc241a61148bb1c44b102
Ellison Domingo <ellisond@nevada.unr.edu>
2024-04-29 21:44:14 +0000

    Ellison - 4/29, 2:44 PM

---
+++ EllisonWork/FirstMenu.c
@@ -0,0 +1,25 @@
+// Author: Ellison Domingo
+// Date: 4/29/24
+// Description: "FirstMenu()" function for final project
+
+#include<stdio.h>
+
+void FirstMenu();
+
+int main()
+{
+	FirstMenu();
+}
+
+// FirstMenu() definition
+// No input parameters, no return, only displays the menu
+void FirstMenu()
+{
+	printf("\nWelcome to the image processing program. Please select an option below.");
+	printf("\n[1] Load new image");
+	printf("\n[2] Display current image");
+	printf("\n[3] Edit current image");
+	printf("\n[4] Exit the program");
+	
+	printf("\n\nEnter the number of your choice here: ");
+}
